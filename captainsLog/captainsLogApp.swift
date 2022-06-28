7f6344945c3c9ad7828b09e1faeae34ca4efddd5
Algenis <algenis.jo@gmail.com>
2022-06-28 13:33:56 -0400

    Initial Commit

---
+++ captainsLog/captainsLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  captainsLogApp.swift
+//  captainsLog
+//
+//  Created by Algenis on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct captainsLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

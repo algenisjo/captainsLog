8d6e1f12d3309ddbbbe12a6c92cf1dfe0a15c322
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2022-08-08 10:58:04 -0400

    Will add a new SwiftUIview - AddNewNoteView - so that users can open up a new page to add their journal entry rather than doing so on the page with the presented saved data.

---
+++ captainsLog/AddNewNoteView.swift
@@ -0,0 +1,43 @@
+//
+//  AddNewNoteView.swift
+//  captainsLog
+//
+//  Created by Algenis Joaquin Ortiz on 8/8/22.
+//
+
+import SwiftUI
+
+struct AddNewNoteView: View {
+    
+    @ObservedObject var myLogBook = LogBook()
+    @Environment(\.dismiss) var dismiss
+    @State private var notes = ""
+    
+    @State private var date = Date.now.formatted(.dateTime.day().month().year())
+    
+    var body: some View {
+        
+        NavigationView{
+            VStack{
+                TextField("Type Here!", text: $notes)
+                Spacer()
+            }
+            .padding()
+            .navigationTitle(date) 
+                .toolbar{
+                    Button("Save"){
+                        let newNote = note(note: notes, date: date)
+                        myLogBook.logBookNotes.append(newNote)
+                        dismiss()
+                    }
+                
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewNoteView()
+    }
+}
--- captainsLog/ContentView.swift
+++ captainsLog/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct note: Identifiable, Codable {
     var id = UUID()
-    let date: String
+    //let date: String
     let note: String
+    let date:  Date.FormatStyle.FormatOutput
 }
 
 
-class logBook: ObservableObject {
+class LogBook: ObservableObject {
     @Published var logBookNotes = [note](){
         didSet {
             //lets save the data using json encoder below
@@ -41,21 +42,14 @@ class logBook: ObservableObject {
 
 struct ContentView: View {
     
-    @StateObject var myLogBook = logBook()
+    @StateObject var myLogBook = LogBook()
     var date = Date.now.formatted(.dateTime.day().month().year())
     @State private var textInput = ""
+    @State private var AddNewNoteViewIsPresented = false
     
     var body: some View {
         NavigationView{
             List{
-                Section("Type below."){
-                    VStack{
-                        TextEditor(text: $textInput)
-                        HStack{
-                            Spacer()
-                        }
-                    }
-                }
                 
                 ForEach(myLogBook.logBookNotes){ item in
                     Section(date){
@@ -73,11 +67,13 @@ struct ContentView: View {
             .navigationBarTitleDisplayMode(.inline)
             .toolbar{
                 ToolbarItem{
-                    Button("Save"){
-                        let newNote = note(date: date, note: textInput)
-                        myLogBook.logBookNotes.append(newNote)
+                    Button("New Note"){
+                        AddNewNoteViewIsPresented = true
                     }
                 }
+            }
+            .sheet(isPresented: $AddNewNoteViewIsPresented){
+                AddNewNoteView()
             }
         }
     }
@@ -90,7 +86,8 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-/// - save the items with user defaults
+/// - Done.------------------ save the items with user defaults
+/// resort  array so that newest item is at the top of the list.
 /// - create a second page to view the list of items. keep the first page for just journaling
 /// - create a way to add images
 /// - change the header to be my username Captain Algenis

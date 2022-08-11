29bf8043566a91f16c7525b3f6fec6cb3e02106a
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2022-08-11 10:43:49 -0400

    Use core data to update my list notes and remove previous list from logbookClass.

--- captainsLog/View/ContentView.swift
+++ captainsLog/View/ContentView.swift
@@ -17,38 +17,68 @@ struct ContentView: View {
     
     @AppStorage("userName") var userName = "Captain's Log"
     
+    //Core Data -------------------------------------------------------------
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var note: FetchedResults<Note>
+    //Core Data -------------------------------------------------------------
+    
     var body: some View {
         NavigationView {
-            List{
-                ForEach(myLogBook.logBookNotes.reversed()){ item in
-                    Section(item.date){
-                        VStack(alignment: .leading){
-                            Text("\(item.note)")
+//            VStack{
+//                List{
+//                    ForEach(myLogBook.logBookNotes.reversed()){ item in
+//                        Section(item.date){
+//                            VStack(alignment: .leading){
+//                                Text("\(item.note)")
+//                                HStack{
+//                                    Spacer()
+//                                }
+//                            }
+//                        }
+//
+//                    }
+//                    .onDelete { offsets in
+//                        let reversed = Array(myLogBook.logBookNotes.reversed())
+//                        let items = Set(offsets.map { reversed[$0].id })
+//                        myLogBook.logBookNotes.removeAll { items.contains($0.id) }
+//                    }
+//                    //                HStack{
+//                    //                    Spacer()
+//                    //                Button("Test Data") {
+//                    //                    let note = Note(context: moc)
+//                    //                    note.id = UUID()
+//                    //                    note.note = "This is a core data note."
+//                    //                    note.date = "d"
+//                    //
+//                    //                    try? moc.save()
+//                    //
+//                    //                }
+//                    //                }
+//                }
+//                Section("testing data"){
+                    List(note) {item in
+                        VStack{
                             HStack{
+                                Text(item.date ?? "Date 1")
                                 Spacer()
                             }
+                            Text(item.entry ?? "Unknown")
                         }
-                    }
-                    
-                }
-                .onDelete { offsets in
-                    let reversed = Array(myLogBook.logBookNotes.reversed())
-                    let items = Set(offsets.map { reversed[$0].id })
-                    myLogBook.logBookNotes.removeAll { items.contains($0.id) }
-                }
+//                    }
+//                }
             }
             .navigationTitle(userName)
             .toolbar{
-                    Button("New Note") {
-                        AddNewNoteViewIsPresented = true
+                Button("New Note") {
+                    AddNewNoteViewIsPresented = true
                 }
             }
             .sheet(isPresented: $AddNewNoteViewIsPresented){
                 AddNewNoteView()
             }
-            .sheet(isPresented: $showSettings){
-                SettingsView()
-            }
+            //            .sheet(isPresented: $showSettings){
+            //                SettingsView()
+            //            }
         }
     }
 }

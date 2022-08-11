6760c83c8b5cc79dcd5b0d222f03dffaebf9dbac
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2022-08-11 10:44:33 -0400

    Implement core data functionality and create a button to add a core data item.

--- captainsLog/View/AddNewNoteView.swift
+++ captainsLog/View/AddNewNoteView.swift
@@ -12,8 +12,10 @@ struct AddNewNoteView: View {
     @ObservedObject var myLogBook = LogBookClass()
     @Environment(\.dismiss) var dismiss
     
+    @Environment(\.managedObjectContext) var moc
+    
     @State private var notes = ""
-    @State private var date = Date.now.formatted(.dateTime.day().month().year())
+    @State private var date = String( Date.now.formatted(.dateTime.day().month().year()) )
     
     var body: some View {
         NavigationView{
@@ -28,11 +30,21 @@ struct AddNewNoteView: View {
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     HStack{
                         Spacer()
-                        Button("Upload."){
-                            let newNote = note(note: notes, date: date)
-                            myLogBook.logBookNotes.append(newNote)
+                        Button("Test Data") {
+                            let note = Note(context: moc)
+                            note.entry = notes
+                            note.date = date
+                            
+                            try? moc.save()
                             dismiss()
                         }
+                        
+//                        Spacer()
+//                        Button("Upload."){
+//                            let newNote = note(note: notes, date: date)
+//                            myLogBook.logBookNotes.append(newNote)
+//                            dismiss()
+//                        }
                     }
                 }
             .navigationTitle(date)

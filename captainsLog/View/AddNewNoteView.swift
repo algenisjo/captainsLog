41340d553ce65d266df35ef2cc5506cf7fe6c984
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2022-08-11 09:15:23 -0400

    Remove comments and whitespace.

--- captainsLog/View/AddNewNoteView.swift
+++ captainsLog/View/AddNewNoteView.swift
@@ -15,63 +15,34 @@ struct AddNewNoteView: View {
     @State private var notes = ""
     @State private var date = Date.now.formatted(.dateTime.day().month().year())
     
-//    @State private var image: Image?
-//    @State private var showingImagePicker = false
-//    @State private var inputImage: UIImage?
-    
-    //    @State private var imageData: Data?
-    
-//    @Environment(\.dismiss) var dismiss
-    
     var body: some View {
         NavigationView{
-//            VStack{
-//                image?
-//                    .resizable()
-//                    .scaledToFit()
-                
                 Form{
                     ZStack(alignment: .leading) {
                         if notes.isEmpty {
                             Text("  Type Here.")
                                 .opacity(notes.isEmpty ? 0.25 : 1)
                         }
-                        
                         TextEditor(text: $notes)
-                        
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     HStack{
                         Spacer()
                         Button("Upload."){
-                            //                        image = Data(Image?)
                             let newNote = note(note: notes, date: date)
-                            //                        let newNote = note(note: notes, date: date, image: imageData)
                             myLogBook.logBookNotes.append(newNote)
                             dismiss()
                         }
                     }
                 }
-//            }
             .navigationTitle(date)
-//            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 Button("Dismiss"){
                     dismiss()
                 }
             }
-
         }
-//        .sheet(isPresented: $showingImagePicker) {
-//            ImagePicker(image: $inputImage)
-//        }
-//        .onChange(of: inputImage) { _ in loadImage() }
     }
-    
-//    func loadImage() {
-//        guard let inputImage = inputImage else { return }
-//        image = Image(uiImage: inputImage)
-//    }
 }
 
 struct AddView_Previews: PreviewProvider {

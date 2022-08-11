94065305ca0314ce41588a0462857565daf5a9da
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2022-08-11 11:59:21 -0400

    Allow users to delete list items.

--- captainsLog/View/ContentView.swift
+++ captainsLog/View/ContentView.swift
@@ -9,47 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @StateObject var myLogBook = LogBookClass()
-//    @State private var textInput = ""
-    
     @State private var AddNewNoteViewIsPresented = false
-//    @State private var showSettings = false
-    
-//    @AppStorage("userName") var userName = "Captain's Log"
     
     //Core Data -------------------------------------------------------------
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: [
-        SortDescriptor(\.entry, order: .reverse)
-    ]) var note: FetchedResults<Note>
+    @FetchRequest(sortDescriptors: []) var note: FetchedResults<Note>
     //Core Data -------------------------------------------------------------
     
     var body: some View {
         NavigationView {
-            //            VStack{
-            //                List(note){ item in
-            //                        Section(item.date){
-            //                            VStack(alignment: .leading){
-            //                                Text("\(item.entry)")
-            //                                HStack{
-            //                                    Spacer()
-            //                                }
-            //                            }
-            //                        }
-            //                    }
-            //                    .onDelete { offsets in
-            //                        let reversed = Array(myLogBook.logBookNotes.reversed())
-            //                        let items = Set(offsets.map { reversed[$0].id })
-            //                        myLogBook.logBookNotes.removeAll { items.contains($0.id) }
-            //                    }
-            //                }
             List{
-                ForEach(note) { item in
+            
+            ForEach(note){ item in
                     Section(item.date ?? "Date 1"){
                             Text(item.entry ?? "Unknown")
                     }
-                }
-                .onDelete(perform: deleteNote)
+            }
+            .onDelete(perform: deleteNote)
             }
             .navigationTitle("Captain's Log")
             .toolbar{
@@ -65,16 +41,15 @@ struct ContentView: View {
     
     func deleteNote(at offsets: IndexSet) {
         for offset in offsets {
-            // find this book in our fetch request
+            // find this note in our fetch request
             let note = note[offset]
-
             // delete it from the context
             moc.delete(note)
         }
-
         // save the context
         try? moc.save()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -82,4 +57,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-

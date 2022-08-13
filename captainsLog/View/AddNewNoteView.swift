17fc7a0c225e0403ec0b8cd8efc97c945b0c5df6
Algenis Joaquin Ortiz <71911185+algenisjo@users.noreply.github.com>
2022-08-13 11:30:04 -0400

    Make textfield start at top: .leading

--- captainsLog/View/AddNewNoteView.swift
+++ captainsLog/View/AddNewNoteView.swift
@@ -18,57 +18,61 @@ struct AddNewNoteView: View {
     
     
     var body: some View {
-        NavigationView{
-            Form{
-                ZStack(alignment: .leading) {
-                    if notes.isEmpty {
-                        Text("  Type Here.")
-                            .opacity(notes.isEmpty ? 0.25 : 1)
-                    }
-                    TextEditor(text: $notes)
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                HStack{
-                    Spacer()
-                    Button("Upload.") {
-                        notes = notes.trimmingCharacters(in: .whitespacesAndNewlines)
-                
-                        if notes.isEmpty {
-                            dismiss()
-                        } else {
-                            let note = Note(context: moc)
-                            note.id = UUID()
-                            note.entry = notes
-                            note.date = date
-                            note.dateOne = Date.now
+            VStack{
+                    ZStack(alignment: .leading) {
+                                   TextEditor(text: $notes)
+                                       .padding()
+                                   if notes.isEmpty {
+                                       VStack{
+                                           Text("  Type Here.")
+                                               .opacity(notes.isEmpty ? 0.25 : 1)
+                                               .padding()
+                                               .padding(.top, 10)
+                                           Spacer()
+                                       }
+                                   }
+                               }
+                               .padding()
+                    HStack{
+                        Spacer()
+                        Button("Upload.") {
+                            notes = notes.trimmingCharacters(in: .whitespacesAndNewlines)
                             
-                            try? moc.save()
-                            dismiss()
+                            if notes.isEmpty {
+                                dismiss()
+                            } else {
+                                let note = Note(context: moc)
+                                note.id = UUID()
+                                note.entry = notes
+                                note.date = date
+                                note.dateOne = Date.now
+                                
+                                try? moc.save()
+                                dismiss()
+                            }
                         }
                     }
+                    .padding()
+                            .navigationTitle(date)
+//                            .navigationBarHidden(true)
+                .toolbar {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "clear.fill")
+                            .font(.body)
+                    }
+                    
                 }
-            }
-            .navigationTitle(date)
-            .toolbar {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "clear.fill")
-                        .font(.body)
-                }
-
-            }
-            .colorScheme(.dark)
-        }
-        
-    }
-    
+                Spacer()
+            } // end of vstack
+        } // end of view
+//    }
 }
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
         AddNewNoteView()
-            .previewInterfaceOrientation(.portrait)
     }
 }
 
